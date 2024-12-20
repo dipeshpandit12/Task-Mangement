1c7c8b51cd8b2b3ce2170bea032f64984d644d5d
Dipesh Pandit <panditdipesh12@gmail.com>
2024-12-20 00:36:03 -0600

    fix: all problem solved

--- task_management/app/editTopic/[id]/page.tsx
+++ task_management/app/editTopic/[id]/page.tsx
@@ -1,9 +1,19 @@
 import EditTopicForm from "@/components/EditTopicForm";
 
-export default function EditTopic() {
-    return (
-        <>
-        <EditTopicForm/>
-        </>
-    );
+const getTopicById=async(id:string)=>{
+    try{
+        const res=await fetch(`http://localhost:3000/api/topics/${id}`, {cache:"no-store"});
+        if(!res.ok) throw new Error("Failed to fetch topic");
+        const data= res.json();
+        return data;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export default async function EditTopic({params}:{params:{id:string}}) {
+    const {id}=params;
+    const {topic}=await getTopicById(id);
+    const {title, description}=topic;
+    return <EditTopicForm id={id} title={title} description={description}/>
 }
