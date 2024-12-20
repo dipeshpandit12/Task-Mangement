787af9adaf78045f7913c2ebe479b1faffb9bade
Dipesh Pandit <panditdipesh12@gmail.com>
2024-12-20 00:23:05 -0600

    fix : error for delete and update

--- task_management/app/api/topics/[id]/route.js
+++ task_management/app/api/topics/[id]/route.js
@@ -4,10 +4,19 @@ import { NextResponse } from "next/server";
 
 export async function PUT(request, { params }) {
     const { id } = params;
-    const { newTitle: title, newDescription: description } = await request.json();
-    await connectToDB();
-    await Topic.findByIdAndUpdate(id, { title, description });
-    return NextResponse.json({ message: "Topic updated successfully" }, { status: 200 });
+    const { title, description } = await request.json();
+
+    try {
+        await connectToDB();
+        await Topic.findByIdAndUpdate(id, { title, description });
+        return NextResponse.json({ message: "Topic updated" }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            { message: "Error updating topic" },
+            { status: 500 }
+        );
+    }
 }
 
 export async function GET(request, { params }) {
@@ -15,4 +24,20 @@ export async function GET(request, { params }) {
     await connectToDB();
     const topic= await Topic.findOne({_id: id});
     return NextResponse.json({topic}, {status: 200});
+}
+
+export async function DELETE(request, { params }) {
+    const { id } = params;
+    
+    try {
+        await connectToDB();
+        await Topic.findByIdAndDelete(id);
+        return NextResponse.json({ message: "Topic deleted" }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            { message: "Error deleting topic" },
+            { status: 500 }
+        );
+    }
 }
